d2d7eb37666a4b6985b37742326a770b49c689f5
LeDrew2017 <ledrew101806@gmail.com>
2022-12-27 16:08:05 -0500

    adapted carbon rom tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_beyondx.mk
+    $(LOCAL_DIR)/carbon_beyondx.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_beyondx-eng \
-    xtended_beyondx-user \
-    xtended_beyondx-userdebug
+    carbon_beyondx-eng \
+    carbon_beyondx-user \
+    carbon_beyondx-userdebug
